37b5a24dbfff40ec14c52d76e4bf5a93fd78f24f
7oops4 <65325376+7oops4@users.noreply.github.com>
2020-06-06 03:01:05 +0900

    Facebookアカウントを用いたログインを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[facebook twitter google_oauth2]
+  # omniauthのコールバック時に呼ばれるメソッド
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 
   validates :name, presence: true, uniqueness: true
 end
