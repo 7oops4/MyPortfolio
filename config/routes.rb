2d4f4f807c283bb4848bc2c4e2d91bc53405dec6
7oops4 <65325376+7oops4@users.noreply.github.com>
2020-06-05 11:13:03 +0900

    アカウント作成・ログイン画面完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # root 'books#show'
-  root 'intls#index'
+  root 'books#index'
   resources :users, only: [:edit, :update]
   resources :books, only: [:index, :new, :create, :edit, :update, :show]
 end
